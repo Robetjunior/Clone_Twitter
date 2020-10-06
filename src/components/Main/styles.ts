94ebb34bb40604e5c794b8ecc86750e929444665
Jose Roberto <juniorferreirarob@gmail.com>
2020-10-05 22:30:05 -0300

    update Main/styles.ts

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { ArrowLeft } from '../../styles/Icons';
 
 export const Container = styled.div`
   display: flex;
@@ -12,6 +13,7 @@ export const Container = styled.div`
 `;
 
 export const Header = styled.div`
+  z-index: 2;
   position: sticky;
   top: 0;
   background: var(--primary);
@@ -38,6 +40,24 @@ export const Header = styled.div`
   }
 `;
 
-export const BackIcon = styled.div``;
+export const BackIcon = styled(ArrowLeft)`
+  width: 24px;
+  height: 24px;
 
-export const ProfileInfo = styled.div``;
+  fill: var(--twitter);
+`;
+
+export const ProfileInfo = styled.div`
+  margin-left: 17px;
+  display: flex;
+  flex-direction: column;
+
+  > strong {
+    font-size: 19px;
+  }
+
+  > span {
+    font-size: 15px;
+    color: var(--gray);
+  }
+`;
