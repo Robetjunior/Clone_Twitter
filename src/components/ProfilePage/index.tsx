16b93db8b135046e8f09b0885eb6ab412ec6e31b
Jose Roberto <juniorferreirarob@gmail.com>
2020-10-06 09:09:23 -0300

    update style ProfilePage

--- src/components/ProfilePage/index.tsx
+++ src/components/ProfilePage/index.tsx
@@ -1,1 +1,54 @@
-rfc;
+import React from 'react';
+
+import {
+  Container,
+  Banner,
+  Avatar,
+  ProfileData,
+  LocationIcon,
+  CakeIcon,
+  FollowAge,
+} from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Container>
+      <Banner>
+        <Avatar />
+      </Banner>
+
+      <ProfileData>
+        {/* <EditButton outlined>Editar perfil</EditButton> */}
+
+        <h1>Jose Roberto</h1>
+        <h2>@joserob__</h2>
+
+        <p>
+          Developer at <a href="https://rocketseat.com.br">Rocketseat</a>
+        </p>
+
+        <ul>
+          <li>
+            <LocationIcon />
+            Sao Paulo, Brasil
+          </li>
+          <li>
+            <CakeIcon />
+            Nascido(a) em 13 de fevereiro de 1997
+          </li>
+        </ul>
+
+        <FollowAge>
+          <span>
+            seguindo <strong>94</strong>
+          </span>
+          <span>
+            <strong>2000</strong> seguidores
+          </span>
+        </FollowAge>
+      </ProfileData>
+    </Container>
+  );
+};
+
+export default ProfilePage;
--- src/components/ProfilePage/styles.ts
+++ src/components/ProfilePage/styles.ts
@@ -1,3 +1,24 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-styled;
+import { LocationOn, Cake } from '../../styles/Icons';
+
+export const Container = styled.div``;
+export const Banner = styled.div``;
+export const Avatar = styled.div``;
+export const ProfileData = styled.div``;
+
+const iconCSS = css`
+  width: 20px;
+  height: 20px;
+
+  color: var(--gray);
+`;
+
+export const LocationIcon = styled(LocationOn)`
+  ${iconCSS}
+`;
+export const CakeIcon = styled(Cake)`
+  ${iconCSS}
+`;
+
+export const FollowAge = styled.div``;
