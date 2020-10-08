aebea51fc0ff63f5b2d71cece67d2fc3bdcf5825
Jose Roberto <juniorferreirarob@gmail.com>
2020-10-08 19:34:28 -0300

    update SideBar/index.tsx

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { 
+  Container, 
+  SearchWrapper, 
+  SearchInput, 
+  SearchIcon, 
+  Body 
+} from './styles';
 
 const SideBar: React.FC = () => {
   return (
     <Container>
-      
+      <SearchWrapper>
+        <SearchInput placeholder="Buscar no Tweeter"/>
+        <SearchIcon />
+      </SearchWrapper>
+
+      <Body>
+        <p>{'Lorem ipsum dolor sit amet.'.repeat(90)}</p>
+      </Body>
     </Container>
   );
 }
