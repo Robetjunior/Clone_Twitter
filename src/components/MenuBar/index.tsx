156110375bc613876fa8626d4837a51994411deb
Jose Roberto <juniorferreirarob@gmail.com>
2020-10-08 19:28:14 -0300

    update style.ts MenuBar

--- src/components/MenuBar/index.tsx
+++ src/components/MenuBar/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Button from '../Button'
+import Button from '../Button';
 
 import { 
   Container, 
@@ -11,7 +11,11 @@ import {
   BellIcon, 
   EmailIcon, 
   FavoriteIcon, 
-  ProfileIcon, 
+  ProfileIcon,
+  Botside,
+  Avatar,
+  ProfileData, 
+  ExitIcon
 } from './styles';
 
 const MenuBar: React.FC = () => {
@@ -50,6 +54,17 @@ const MenuBar: React.FC = () => {
          </Button> */}
 
       </Topside>
+
+      <Botside>
+        <Avatar />
+
+        <ProfileData>
+          <strong>Jose Roberto</strong>
+          <span>@joserob__</span>
+        </ProfileData>
+
+        <ExitIcon /> 
+      </Botside>
     </Container>
   );
 }
