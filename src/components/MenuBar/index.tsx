5d397da11fedd0bb49357ef2fc522e7a8679243c
Jose Roberto <juniorferreirarob@gmail.com>
2020-10-08 17:35:23 -0300

    update MenuBar

---
+++ src/components/MenuBar/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import Button from '../Button'
+
+import { Container, 
+  Topside,
+  Logo, 
+  MenuBotton, 
+  HomeIcon, 
+  BellIcon, 
+  EmailIcon, 
+  FavoriteIcon, 
+  ProfileIcon, 
+} from './styles';
+
+const MenuBar: React.FC = () => {
+  return (
+    <Container>
+      <Topside>
+         <Logo />
+
+         <MenuBotton>
+           <HomeIcon />
+           <span>Página Inicial</span>
+         </MenuBotton>
+
+         <MenuBotton>
+           <BellIcon />
+           <span>Notificações</span>
+         </MenuBotton>
+
+         <MenuBotton>
+           <EmailIcon />
+           <span>Mensagens</span>
+         </MenuBotton>
+
+         <MenuBotton>
+           <FavoriteIcon />
+           <span>Favoritados</span>
+         </MenuBotton>
+
+         <MenuBotton className="active">
+           <ProfileIcon />
+           <span>Perfil</span>
+         </MenuBotton>
+
+         <Button>
+           <span>Twittar</span>
+         </Button>
+
+      </Topside>
+    </Container>
+  );
+}
+
+export default MenuBar;
--- src/components/MenuBar/styles.ts
+++ src/components/MenuBar/styles.ts
@@ -1,5 +1,25 @@
 import styled from 'styled-components';
 
+import { Home, Notifications, Email, FavoriteBorder, Person, ExitToApp } from '../../styles/Icons'
+
 export const Container = styled.div`
   
 `;
+
+export const Topside = styled.div``;
+
+export const Logo = styled.div``;
+
+export const MenuBotton = styled.div``;
+
+
+
+export const HomeIcon = styled();
+
+export const BellIcon = styled();
+
+export const EmailIcon = styled();
+
+export const FavoriteIcon = styled();
+
+export const ProfileIcon= styled();
